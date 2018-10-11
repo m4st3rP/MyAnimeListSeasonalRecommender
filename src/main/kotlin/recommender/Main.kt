ee7fad6f19c438930b95868d9aa17ea5e693bf65
Philipp Schwarz <schwarz_philipp@hotmail.de>
2018-10-11 19:01:21 +0200

    added file download

--- src/main/kotlin/recommender/Main.kt
+++ src/main/kotlin/recommender/Main.kt
@@ -1,10 +1,10 @@
 package recommender
 
 fun main(args: Array<String>) {
-    var input: String? = ""
+    var input: String? = "masterP" // TODO change back to blank String
     while (input == null) {
         println("Enter MAL username!")
-        input = readLine()
+        input = readLine() // TODO fix input
     }
     val recommender = Recommender(input)
 
--- src/main/kotlin/recommender/Recommender.kt
+++ src/main/kotlin/recommender/Recommender.kt
@@ -1,14 +1,24 @@
 package recommender
 
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import java.io.File
+import java.io.FileOutputStream
 
-class Recommender(username: String) {
-    var animeMap: MutableMap<Int, Double> = HashMap()
-    var genreMap: MutableMap<String, Double> = HashMap()
-    var studioMap: MutableMap<String, Double> = HashMap()
-    var staffMap: MutableMap<String, Double> = HashMap()
+class Recommender(private var username: String) {
+    private var animeMap: MutableMap<Int, Double> = HashMap() // Key is ID of an anime and Value is the users score
+    private var genreMap: MutableMap<String, Double> = HashMap()
+    private var studioMap: MutableMap<String, Double> = HashMap()
+    private var staffMap: MutableMap<String, Double> = HashMap()
 
+    /**
+     * This method downloads a users animelist and puts the anime ID and the users score into the animeMap.
+     */
     fun analyzeAnimelistJSON() {
+        // download JSON
+        downloadFile("https://api.jikan.moe/v3/user/$username/animelist", "animelist.json")
+
+        // extract data from JSON
         val json = File("animelist.json") // "https://api.jikan.moe/v3/user/$username/animelist"
         val jsonAsString = json.readText().substringAfter("\"anime\":[{\"") // delete the beginning before the animes
         val animes = jsonAsString.split("mal_id\":") // split between the animes
@@ -21,12 +31,24 @@ class Recommender(username: String) {
 
             if (score > 0.0) {
                 animeMap[id] = score
+                //println("$id: $score")
             }
         }
+    }
+
+    fun analyzeAnimeMap() {
 
     }
 
-    fun analyeAnimeMap() {
-
+    private fun downloadFile(url: String, name: String) {
+        val client = OkHttpClient()
+        val request = Request.Builder().url(url).build()
+        val response = client.newCall(request).execute()
+        /*if (!response.isSuccessful()) {
+            throw IOException("Failed to download file: " + response)
+        }*/
+        val fos = FileOutputStream(name)
+        fos.write(response.body()?.bytes())
+        fos.close()
     }
 }
